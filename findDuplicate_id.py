83783209e73e41290cbc44174008e7012a525af2
Aatish <aatishk@yahoo-inc.com>
2016-08-04 21:48:49 -0700

    Users with duplicate uid

---
+++ findDuplicate_id.py
@@ -0,0 +1,15 @@
+import collections
+
+file = open('/Users/aatishk/PycharmProjects/python_files/passwd')
+pass_file = file.read().split()
+
+pass_dict = collections.defaultdict(list)
+
+for line in pass_file:
+    user = line.split(':')[0]
+    uid = line.split(':')[2]
+    pass_dict[uid].append(user)
+
+for k,v in pass_dict.items():
+    if len(v) > 1:
+        print "Users" , ", ".join(v) , "have duplicate uid:" , k
