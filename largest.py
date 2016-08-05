f51f64736fbde50091179344eee63c90ca28b3f6
aatishraj <aatishraj@gmail.com>
2016-08-04 19:56:01 -0700

    Find largest number in list

---
+++ largest.py
@@ -0,0 +1,11 @@
+myList = [5, 2, 3, 9, 10, 7, 8, 15, 20, 13, 1, 4, 6, 9]
+
+large_seen = None
+
+for number in myList:
+    if large_seen is None or large_seen < number:
+        large_seen = number
+    else:
+        continue
+
+print large_seen
